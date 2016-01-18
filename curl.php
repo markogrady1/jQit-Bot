b78e69c6adcac6d12a0dc1b5a97cea257e493d7a
Mark O Grady <markogrady.code@gmail.com>
2016-01-18 16:18:05 +0000

    Update curl.php

--- curl.php
+++ curl.php
@@ -96,20 +96,71 @@ class Curl {
 				echo "Curl Error: " . curl_error($curl);
 		}
 	}
+	
+	public function getIssueHistoryCurl($url, $page, $openPrArray) {
+		$curl = curl_init($url);
 
-    public function assignClient($url, $page, $state = ''){
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array("User-Agent: php-curl"));
+
+
+		$response = curl_exec($curl);
+		$response = $response . "\n";
+		$info = curl_getinfo($curl);
+
+		if($info['http_code'] == 200) {
+
+		curl_close($curl);
+
+		$data = json_decode($response, true);
+		$repoName = array();
+		$p = 0;
+		$filename = 'repo_history/repo_issue_history.txt';
+
+		$repoName = array();
+		$i = 0;
+		$repoString = "";
+
+		$date = new DateTime();
+		$isoFormat =  $date->format('c = U');
+		$isoFormat = split(' = ', $isoFormat);
+		$isoFormat = substr($isoFormat[0], 0, 19);
+		$isoFormat = $isoFormat . 'Z';
+
+		if($page == 1) {
+		$today = "*". $isoFormat . " = " .$date->format('U') .',' . PHP_EOL;
+		$handle = fopen($filename, 'a+');
+				fwrite($handle, $today);
+		}
+		foreach ($data as $key => $value) {
+
+			$repoName[$i]['name'] = $value['name'];
+			$repoName[$i]['issues'] = intval(['open_issues']) - intval($openPrArray[$p++]);
+			$repoString = $repoName[$i]['name'] . "  " . $repoName[$i++]['issues'] . "," . PHP_EOL;
+				$handle = fopen($filename, 'a+');
+				fwrite($handle, $repoString);			
+		}						
+					fclose($handle);
+		} else {
+
+			echo "Curl Error: " . curl_error($curl);
+		}
+    	}
+	
+    	public function assignClient($url, $page, $state = '') {
 
 		$url = str_replace("{/number}", "", $url);
 
 		$url = $url . "?client_id=" . ID . "&client_secret=" . SEC . "" . $state . "" . $page . "";
 		
 		return $url;	
-    }
+    	}
 
-    public function toJSONArray($data) {
+    	public function toJSONArray($data) {
     	$rawData1 = preg_replace("/^\[/", "", $data);
 		$rawData1 = preg_replace("/\]$/", ",", $rawData1);
 
 		return $rawData1;
-    }
+    	}
 }
